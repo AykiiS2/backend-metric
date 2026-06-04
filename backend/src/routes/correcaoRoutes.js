4b89e69707911c87a761acef1dbcf44951187d07
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-04 20:10:05 -0300

    Delete backend directory

--- backend/src/routes/correcaoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { corrigirImagem, corrigirTexto, upload } = require('../controllers/correcaoController');
-
-router.post('/imagem', upload.single('imagem'), corrigirImagem);
-router.post('/texto', corrigirTexto);
-
-module.exports = router;
