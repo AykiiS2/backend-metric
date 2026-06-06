38b742e969703bfe54c1ba22d0293e81b2d37b58
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-06 18:39:34 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const escolasRoutes = require('./escolaRoutes');
 const turmasRoutes = require('./turmaRoutes');
 const alunosRoutes = require('./alunoRoutes');
 const authRoutes = require('./authRoutes');
+const rankingRoutes = require('./rankingRoutes');
 
 router.use('/escolas', escolasRoutes);
 router.use('/turmas', turmasRoutes);
 router.use('/alunos', alunosRoutes);
 router.use('/auth', authRoutes);
+router.use('/ranking', rankingRoutes);
 
 module.exports = router;
