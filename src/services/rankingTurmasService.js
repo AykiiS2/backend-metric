ed46f5a6ae1c787232b6d16b19deeca2ae3c49c1
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-07 20:57:47 -0300

    Update rankingTurmasService.js

--- src/services/rankingTurmasService.js
+++ src/services/rankingTurmasService.js
@@ -20,6 +20,21 @@ class RankingTurmasService {
   async obterRanking(escolaId) {
     return await RankingTurmasModel.obterRanking(escolaId);
   }
+
+  async obterMelhorTurma(escolaId) {
+    const ranking = await RankingTurmasModel.obterRanking(escolaId);
+    if (!ranking || ranking.length === 0) return null;
+    
+    const melhor = ranking[0];
+    return {
+      turmaId: melhor.turma_id,
+      nomeTurma: melhor.nome_turma,
+      escolaId: melhor.escola_id,
+      mediaPercentual: melhor.media_percentual,
+      quantidadeAlunos: melhor.quantidade_alunos,
+      posicao: 1
+    };
+  }
 }
 
 module.exports = new RankingTurmasService();
