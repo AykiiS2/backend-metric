58729029a08f335c9cd2b2970969fdeb68e027fd
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-07 16:14:29 +0000

    Update rankingService.js

--- src/services/rankingService.js
+++ src/services/rankingService.js
@@ -2,7 +2,7 @@ const RankingModel = require('../models/Ranking');
 
 class RankingService {
   async atualizarRanking(dados) {
-    const { alunoId, escolaId, turmaId, nome, pontuacao } = dados;
+    const { alunoId, escolaId, turmaId, nome, codIdentificacao, pontuacao, tempo } = dados;
     
     if (!alunoId || !escolaId || !turmaId || !nome) {
       throw new Error('Dados incompletos para atualizar ranking');
@@ -12,7 +12,21 @@ class RankingService {
       throw new Error('Pontuação inválida');
     }
     
-    return await RankingModel.atualizarRanking(dados);
+    return await RankingModel.atualizarRanking({
+      alunoId, escolaId, turmaId, nome, codIdentificacao, pontuacao, tempo
+    });
+  }
+
+  async registrarHistorico(dados) {
+    const { alunoId, codIdentificacao, pontuacao, acertos, erros, tempoSegundos } = dados;
+    
+    if (!alunoId || !codIdentificacao) {
+      throw new Error('Dados incompletos para registrar histórico');
+    }
+    
+    return await RankingModel.registrarHistorico({
+      alunoId, codIdentificacao, pontuacao, acertos, erros, tempoSegundos
+    });
   }
 
   async obterRanking(filtros) {
