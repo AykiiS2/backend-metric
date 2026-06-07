7392b82a27d25edb0bd6cf0eb299c139bb249c40
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-07 20:32:04 -0300

    Update rankingEscolasController.js

--- src/controllers/rankingEscolasController.js
+++ src/controllers/rankingEscolasController.js
@@ -3,13 +3,20 @@ const rankingEscolasService = require('../services/rankingEscolasService');
 class RankingEscolasController {
   async atualizar(req, res, next) {
     try {
-      const { escolaId, nomeEscola, notaConceitual } = req.body;
+      const { escolaId, nomeEscola, mediaPercentual, quantidadeTurmas, quantidadeAlunos } = req.body;
       
       if (!escolaId || !nomeEscola) {
         return res.status(400).json({ error: 'Dados incompletos' });
       }
       
-      const resultado = await rankingEscolasService.atualizarDesempenho(escolaId, nomeEscola, notaConceitual);
+      const resultado = await rankingEscolasService.atualizarRanking({
+        escolaId,
+        nomeEscola,
+        mediaPercentual: mediaPercentual || 0,
+        quantidadeTurmas: quantidadeTurmas || 0,
+        quantidadeAlunos: quantidadeAlunos || 0
+      });
+      
       res.json({ success: true, data: resultado });
     } catch (error) {
       next(error);
