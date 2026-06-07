bf15afdeaeb5829cd64218dfb46377bc47918254
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-07 15:04:13 -0300

    Delete src directory

--- src/routes/rankingRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const rankingController = require('../controllers/rankingController');
-
-router.post('/atualizar', rankingController.atualizarRanking);
-router.get('/obter', rankingController.obterRanking);
-router.get('/posicao/:alunoId', rankingController.obterPosicaoAluno);
-
-module.exports = router;
