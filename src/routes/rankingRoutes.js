c01ee2e90700474d766f417e1e1bb74fc500e892
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-07 16:22:48 +0000

    Update rankingRoutes.js

--- src/routes/rankingRoutes.js
+++ src/routes/rankingRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const rankingController = require('../controllers/rankingController');
-const { authMiddleware } = require('../middlewares/tokenMiddleware');
+const { tokenMiddleware } = require('../middlewares/tokenMiddleware');
 
-router.post('/atualizar', authMiddleware, rankingController.atualizarRanking);
-router.post('/historico', authMiddleware, rankingController.registrarHistorico);
+router.post('/atualizar', tokenMiddleware, rankingController.atualizarRanking);
+router.post('/historico', tokenMiddleware, rankingController.registrarHistorico);
 router.get('/obter', rankingController.obterRanking);
 router.get('/posicao/:alunoId', rankingController.obterPosicaoAluno);
 
