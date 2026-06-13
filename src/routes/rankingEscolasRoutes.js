9b93825d0ab6c66fbc8700aefe92812e55b43010
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-06-13 18:18:44 -0300

    Update rankingEscolasRoutes.js

--- src/routes/rankingEscolasRoutes.js
+++ src/routes/rankingEscolasRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const rankingEscolasController = require('../controllers/rankingEscolasController');
 const { tokenMiddleware } = require('../middlewares/tokenMiddleware');
 
+router.get('/', rankingEscolasController.obterRanking);
 router.post('/atualizar', tokenMiddleware, rankingEscolasController.atualizar);
-router.get('/obter', tokenMiddleware, rankingEscolasController.obterRanking);
-router.get('/melhor', tokenMiddleware, rankingEscolasController.obterMelhorEscola);
+router.get('/melhor', rankingEscolasController.obterMelhorEscola);
 
 module.exports = router;
